6521eba24f0d84df0303262c39289d34aa9d8cfe
Brandt Ryan <emailme@brandtryan.com>
2014-05-30 07:47:08 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sodibee::Application.config.secret_token = '9ed445dd37a9abbe5ed3a882aac37aca5941d28cff96065bbadcdbde855de5a4250be3a37769b99c0c0fdcad5bc99ad4bf06c6404653dbe85dce19f3bc3d7c88'
